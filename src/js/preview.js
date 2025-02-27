4d221b9eee934593f82bbd95ef19914346b4cfc5
AndreaChust <andrea.chust@hotmail.com>
2025-02-27 13:26:03 +0100

    form y objets

--- src/js/preview.js
+++ src/js/preview.js
@@ -12,15 +12,50 @@ const previewDesperation = document.querySelector(".js-desperation-preview");
 const previewTxt = document.querySelector(".js-txt-preview");
 const previewAuthor = document.querySelector(".js-author-preview");
 
+const formData = {
+    image: "",
+    desperation: 0,
+    sentence: "",
+    author: "",
+    size: 0,
+    colors: "",
+}
+
 function getPreview(event) {
+    const select  = "Selecciona tu frase";
+    const process  = "Confía en el proceso";
+    const reverse = "Me siento un poco column-reverse";
+    const crying = "Crying in JavaScript";
+
+
     if (event.target.id === "image") {
         previewImg.innerHTML = event.target.value;
+        formData.image = event.target.value;
     } else if (event.target.id === "desperation") {
         previewDesperation.innerHTML = event.target.value;
+        formData.desperation = event.target.value;
     } else if (event.target.id === "sentence") {
-        previewTxt.innerHTML = event.target.value;
+        // formData.sentence = event.target.options[parseInt(event.target.value)].text;
+        // previewTxt.innerHTML = event.target.options[parseInt(event.target.value)].text;
+        //  ¡¡¡ FORMA MáS AVANZADA DE HACERLO!!!
+        if (event.target.value === "select") {
+            console.log(event.target.options);
+            previewTxt.innerHTML = select;
+            formData.sentence = select;
+        } else if (event.target.value === "process") {
+            previewTxt.innerHTML = process;
+            formData.sentence = process;
+        } else if (event.target.value === "reverse") {
+            previewTxt.innerHTML = reverse;
+            formData.sentence = reverse;
+        } else if (event.target.value === "crying") {
+            previewTxt.innerHTML = crying;
+            formData.sentence = crying;
+        }
     } else if (event.target.id === "author") {
-        previewAuthor.innerHTML = event.target.value;
+        previewAuthor.innerHTML = "Creado por " + event.target.value;
     }
+    
 }
 fillBox.addEventListener("input", getPreview);
+
