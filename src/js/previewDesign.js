d0a0901b5b260ef53a66330ba4b9118893a0463e
AndreaChust <andrea.chust@hotmail.com>
2025-03-03 14:02:38 +0100

    finish colors

--- src/js/previewDesign.js
+++ src/js/previewDesign.js
@@ -9,22 +9,51 @@
 */
 
 const inputSize = document.querySelector(".js-size");
+const inputColor = document.querySelector(".js-color");
 // La constante previewTxt la creamos en previewFill.js
 
 const changeClassSentence = (event) => {
     if (event.target.value === "small") {
         previewTxt.classList.add("smallSize");
-        previewTxt.classList.remove("mediumSize");
-        previewTxt.classList.remove("bigSize");
+        previewTxt.classList.remove("mediumSize", "bigSize");
     } else if (event.target.value === "medium") {
         previewTxt.classList.add("mediumSize");
-        previewTxt.classList.remove("smallSize");
-        previewTxt.classList.remove("bigSize");
+        previewTxt.classList.remove("smallSize", "bigSize");
     } else if (event.target.value === "big") {
         previewTxt.classList.add("bigSize");
-        previewTxt.classList.remove("mediumSize");
-        previewTxt.classList.remove("smallSize");
+        previewTxt.classList.remove("mediumSize", "smallSize");
     }
 };
 
 inputSize.addEventListener("input", changeClassSentence);
+
+/*
+Seleccionar el input de colores, nivel de desesperacion y la frase escogida (ya tenemos desperation y la frase)
+Crear las clases de los colores.
+Cuando la usuaria escoja el input:
+ - Si elige la opcion 1 que sea del color patata
+ - Si elige la opcion 2 que sea del color patata 2
+ - Si elige la opcion 3 que sea del color patata 3.
+*/
+
+const changeColor = (ev) => {
+        if (ev.target.value === "option1") {
+            previewTxt.classList.add("color1");
+            previewDesperation.classList.add("background1");
+            previewTxt.classList.remove("color2", "color3");
+            previewDesperation.classList.remove("background2", "background3");
+        } else if (ev.target.value === "option2") {
+            previewTxt.classList.add("color2");
+            previewDesperation.classList.add("background2");
+            previewTxt.classList.remove("color1", "color3");
+            previewDesperation.classList.remove("background1", "background3");
+        } else if (ev.target.value === "option3") {
+            previewTxt.classList.add("color3");
+            previewDesperation.classList.add("background3");
+            previewTxt.classList.remove("color1", "color2");
+            previewDesperation.classList.remove("background1", "background2");
+        }
+
+}
+
+inputColor.addEventListener("input", changeColor);
